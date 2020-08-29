79cc83d0ff4e836542564690dec5423b95c25fa7
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-08-29 11:36:58 +0200

    add helpers for static points

--- src/js/Assets.js
+++ src/js/Assets.js
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three';
+import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
 import Scene from './core/Scene.js';
 
@@ -37,6 +38,72 @@ function makeIco( faceSub ) {
 
 	})
 
+	// add ID to each vertex
+
+	geometry.vertices.forEach( (vec, i) => {
+
+		vec.id = i
+
+	});
+
+	// add adjacent vertices information to each vertex
+
+	geometry.vertices.forEach( (thisVertex, i) => {
+
+		thisVertex.adjVertices = [];
+
+		geometry.faces.forEach( (face) => {
+
+			const faceVertIDs = [ face.a, face.b, face.c ];
+
+			if ( faceVertIDs.includes( i ) ) {
+				
+				faceVertIDs.forEach( (vertIDToAdd) => {
+
+					if ( vertIDToAdd === i ) return
+
+					if ( ! thisVertex.adjVertices.includes( geometry.vertices[ vertIDToAdd ] ) ) {
+
+						thisVertex.adjVertices.push( geometry.vertices[ vertIDToAdd ] )
+
+					}
+					
+				})
+
+			}
+
+		})
+
+	})
+
+	// add helpers for static points
+
+	const HELPER_RAD = 0.02;
+
+	const geoms = [];
+
+	geometry.vertices.forEach( (vec) => {
+
+		if ( vec.adjVertices.length === 5 ) {
+
+			const newGeom = new THREE.IcosahedronBufferGeometry( HELPER_RAD, 1 );
+
+			newGeom.translate( vec.x, vec.y, vec.z );
+
+			geoms.push( newGeom );
+
+		}
+
+	})
+
+	const helperGeometry = BufferGeometryUtils.mergeBufferGeometries( geoms );
+
+	const helperMaterial = new THREE.MeshBasicMaterial();
+
+	const helper = new THREE.Mesh( helperGeometry, helperMaterial )
+
+	Scene.add( helper )
+
 	//
 
 	const mesh = new THREE.Mesh( geometry, icoMaterial );
@@ -47,4 +114,4 @@ function makeIco( faceSub ) {
 
 //
 
-Scene.add( makeIco( 1 ) );
+Scene.add( makeIco( 2 ) );
